f07ab25e9495e5375c34e9f13e409c70bd2367b9
Salla Koskinen <salla.a.koskinen@mehilainen.fi>
2021-04-16 20:34:22 +0300

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,28 @@
+require('dotenv').config()
+
+module.exports = {
+  siteMetadata: {
+    title: "Salla's site",
+  },
+  plugins: [
+    {
+      resolve: 'gatsby-source-sanity',
+      options: {
+        projectId: process.env.SANITY_ID,
+        dataset: '',
+      },
+    },
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-image',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: './src/images/',
+      },
+      __key: 'images',
+    },
+  ],
+}
